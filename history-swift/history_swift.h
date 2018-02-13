629717aecec4160d125bc930aa5146225827e662
Helior Colorado <me@helior.info>
2018-02-12 17:23:07 -0800

    Initial xcode project commit

---
+++ history-swift/history_swift.h
@@ -0,0 +1,19 @@
+//
+//  history_swift.h
+//  history-swift
+//
+//  Created by Helior Colorado on 2/12/18.
+//  Copyright © 2018 Helior Colorado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for history_swift.
+FOUNDATION_EXPORT double history_swiftVersionNumber;
+
+//! Project version string for history_swift.
+FOUNDATION_EXPORT const unsigned char history_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <history_swift/PublicHeader.h>
+
+
