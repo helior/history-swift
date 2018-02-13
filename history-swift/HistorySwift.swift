ca96e12aabe4fe727b9deb5f1a48b9a9378aa0b0
Helior Colorado <me@helior.info>
2018-02-13 15:42:52 -0800

    Basic library code.

---
+++ history-swift/HistorySwift.swift
@@ -0,0 +1,26 @@
+//
+//  HistorySwift.swift
+//  history-swift
+//
+//  Created by Helior Colorado on 2/13/18.
+//  Copyright © 2018 Helior Colorado. All rights reserved.
+//
+
+import Foundation
+
+class HistorySwift {
+    static let shared = HistorySwift()
+    private var values = [String]()
+    
+    private init() {
+        
+    }
+    
+    func addValue(item:String) {
+        self.values.append(item)
+    }
+    
+    func getValues() -> [String] {
+        return self.values
+    }
+}
